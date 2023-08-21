a7aafe97d948c65d30552be9fa5d6aaf375453a7
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-08-21 14:27:51 +0200

    Install redux toolkit

---
+++ src/icons.js
@@ -0,0 +1,61 @@
+export const CartIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6 "
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    strokeWidth={2}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+    />
+  </svg>
+);
+
+export const ChevronDown = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    strokeWidth={2}
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+  </svg>
+);
+
+export const ChevronUp = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    strokeWidth={2}
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" d="M5 15l7-7 7 7" />
+  </svg>
+);
+
+// export const Testing = () => (
+//   <svg
+//     xmlns="http://www.w3.org/2000/svg"
+//     fill="none"
+//     viewBox="0 0 24 24"
+//     strokeWidth={1.5}
+//     stroke="currentColor"
+//     className="w-6 h-6"
+//   >
+//     <path
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//       d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5
+// 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112
+// 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+//     />
+//   </svg>
+// );
